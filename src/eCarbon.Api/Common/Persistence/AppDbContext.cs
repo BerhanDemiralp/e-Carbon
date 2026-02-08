6ab7b424c4b77ccfa4d437b16d690aab399e83cc
Berhan <demiralpberhan@gmail.com>
2026-02-09 00:58:29 +0300

    Fix: DateTime UTC error for Frontend

--- src/eCarbon.Api/Common/Persistence/AppDbContext.cs
+++ src/eCarbon.Api/Common/Persistence/AppDbContext.cs
@@ -1,7 +1,15 @@
 using eCarbon.Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace eCarbon.Api.Common.Persistence;
+
+public static class UtcDateTimeConverter
+{
+    public static readonly ValueConverter<DateTime, DateTime> ValueConverter = new(
+        v => v.Kind == DateTimeKind.Utc ? v : v.ToUniversalTime(),
+        v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+}
 
 public class AppDbContext : DbContext
 {
@@ -21,6 +29,18 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(DateTime) || property.ClrType == typeof(DateTime?))
+                {
+                    property.SetValueConverter(UtcDateTimeConverter.ValueConverter);
+                }
+            }
+        }
+        
         base.OnModelCreating(modelBuilder);
     }
 }
